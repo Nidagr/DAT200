f7f0baf7921c0fa858f6d532fc6cad1d4628c946
Nidagr <nidamariag@gmail.com>
2024-01-26 10:16:33 +0100

    Second compulsary assignment

---
+++ CA2/adaline.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 20:32:04 2018
+
+@author: olto
+"""
+
+import numpy as np
+
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=5, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        # initialize random weights for the variables.
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        # create list that will contain the cost.
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            # call the net_input function to calculate sum(W*X)
+            net_input = self.net_input(X)
+            """
+            Please note that the "activation" method has no effect
+            in the code since it is simply an identity function. We
+            could write `output = self.net_input(X)` directly instead.
+            The purpose of the activation is more conceptual, i.e.,  
+            in the case of logistic regression (as we will see later), 
+            we could change it to
+            a sigmoid function to implement a logistic regression classifier.
+            """
+            # output phi(W*X)=W*X
+            output = self.activation(net_input)
+            # compute the errors y-phi(w*x)
+            errors = (y - output)
+            # the new weights are the old weights + eta*gradient of cost
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            # calculate the cost
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input. sum(W*X)"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation. phi(W*X)=W*X. input=output."""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+
+
+
